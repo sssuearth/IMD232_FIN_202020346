f4f524aa22d999f38ef43adf928a25a4efca4a22
sssuearth <128416382+sssuearth@users.noreply.github.com>
2023-12-18 23:02:19 +0900

    sui final

--- script/sketch.js
+++ script/sketch.js
@@ -1,3 +1,6 @@
+// https://brm.io/matter-js/demo/#sprites
+// Matter.js Demo - Sprites 예제 사용, 변형
+
 // 매터 쓰기 위한 기본 변수
 var Engine = Matter.Engine,
   Render = Matter.Render,
@@ -9,11 +12,11 @@ var Engine = Matter.Engine,
   Composite = Matter.Composite,
   Bodies = Matter.Bodies;
 
-// 엔진 만들기
+// 엔진
 var engine = Engine.create({ gravity: { x: 0, y: 0 } }),
   world = engine.world;
 
-// 러너 만들고 실행하기
+// 러너 만들고 실행
 var runner = Runner.create();
 Runner.run(runner, engine);
 
@@ -40,6 +43,7 @@ let box11Images = [];
 let box12Images = [];
 
 // 현재까지 몇 번째 이미지를 선택했는지 추적하는 변수
+// 이미지 중복 배열 방지
 let box1ImageIndex1 = 0;
 let box2ImageIndex2 = 0;
 let box3ImageIndex3 = 0;
@@ -54,6 +58,7 @@ let box11ImageIndex11 = 0;
 let box12ImageIndex12 = 0;
 
 // preload 함수에서 이미지 로드
+// 그룹별(1줄당) 세트 이미지들 한꺼번에 불러오기
 function preload() {
   for (let i = 4; i <= 8; i++) {
     box1Images.push(loadImage(`./3dpng/0-${i}.png`));
@@ -82,17 +87,20 @@ function preload() {
   for (let i = 58; i <= 63; i++) {
     box9Images.push(loadImage(`./3dpng/0-${i}.png`));
   }
+  //컬러박스
   for (let i = 65; i <= 66; i++) {
     box10Images.push(loadImage(`./3dpng/0-${i}.png`));
   }
+  //눈금박스
   for (let i = 67; i <= 68; i++) {
     box11Images.push(loadImage(`./3dpng/0-${i}.png`));
   }
+  //9개 텍스트
   for (let i = 1; i <= 9; i++) {
     box12Images.push(loadImage(`./3dpng/1-${i}.png`));
   }
 
-  // 이미지 배열 랜덤 셔플
+  // 이미지 배열을 랜덤으로 불러오기 위한 셔플
   shuffleArray(box1Images);
   shuffleArray(box2Images);
   shuffleArray(box3Images);
@@ -107,6 +115,8 @@ function preload() {
   shuffleArray(box12Images);
 }
 
+// 이미지 배열 랜덤으로 섞기 함수
+// ChatGPT에게 질문..
 function shuffleArray(array) {
   for (let i = array.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1));
@@ -130,7 +140,9 @@ function setup() {
 
   // 도형과 이미지
   stack = Composite.create();
-  // 생성할 사각형 갯수
+
+  // 그룹 별(줄) 생성할, 이미지 넣을 사각형 갯수
+  // 1~9까지 드래그 개체
   const numRectangles1 = 5;
   const numRectangles2 = 6;
   const numRectangles3 = 7;
@@ -140,39 +152,44 @@ function setup() {
   const numRectangles7 = 7;
   const numRectangles8 = 7;
   const numRectangles9 = 6;
-  const numRectangles10 = 2;
-  const numRectangles11 = 1;
-  const numRectangles12 = 9;
+  const numRectangles10 = 2; //컬러박스 2개
+  const numRectangles11 = 1; //눈금박스 1개
+  const numRectangles12 = 9; //오른쪽 텍스트 9개
 
   //사각형에 이미지 넣기
-  //11번 눈금박스 2개 //맨뒤
+  //
+  // 11번 눈금박스 2개 //맨뒤
   for (let i = 0; i < numRectangles11; i++) {
-    const x = 805 + i * 1; // x 위치를 고정
-    const y = 435; // y 위치를 조절하여 15씩 차이 나게 설정
+    const x = 805 + i * 1;
+    const y = 435;
 
     Composite.add(
       stack,
       Bodies.rectangle(x, y, 30, 30, {
-        isStatic: true,
+        isStatic: true, //고정
         render: {
           sprite: {
             texture: box11Images[box11ImageIndex11 % box11Images.length],
+            // 이미지 중복을 방지하기 위해 배열 크기로 나눔
+            // 'box11ImageIndex11'을 사용해 'box11Images' 배열에서 이미지를 가져온다.
+            // '% box11Images.length'를 사용하여 배열의 범위를 벗어나지 않도록 한다.
           },
         },
       })
     );
-
     box11ImageIndex11++;
+    // 이미지 인덱스를 증가시켜 다음 바디에서 중복되지 않는 이미지를 사용하도록 한다.
   }
-  //12번 글자박스 9개 //맨뒤
+
+  // 12번 글자박스 9개 //맨뒤
   for (let i = 0; i < numRectangles12; i++) {
-    const x = 50; // x 위치를 고정
-    const y = 100 + i * 68; // y 위치를 조절하여 15씩 차이 나게 설정
+    const x = 50;
+    const y = 100 + i * 68;
 
     Composite.add(
       stack,
       Bodies.rectangle(x, y, 30, 30, {
-        isStatic: true,
+        isStatic: true, //고정
         render: {
           sprite: {
             texture: box12Images[box12ImageIndex12 % box12Images.length],
@@ -180,12 +197,12 @@ function setup() {
         },
       })
     );
-
     box12ImageIndex12++;
   }
-  //1줄 5개
+
+  // 1줄 5개
   for (let i = 0; i < numRectangles1; i++) {
-    const x = 158 + i * 160; // x 위치를 고정
+    const x = 158 + i * 160;
     const y = 50;
 
     Composite.add(
@@ -193,167 +210,167 @@ function setup() {
       Bodies.rectangle(x, y, 100, 150, {
         render: {
           sprite: {
-            texture: box1Images[box1ImageIndex1 % box1Images.length], // 중복을 방지하기 위해 배열 크기로 나눔
+            texture: box1Images[box1ImageIndex1 % box1Images.length],
           },
         },
       })
     );
-
     box1ImageIndex1++;
   }
-  //2줄 7개
+
+  // 2줄 7개
   for (let i = 0; i < numRectangles2; i++) {
-    const x = 220 + i * 115; // x 위치를 고정
-    const y = 235; // y 위치를 조절하여 15씩 차이 나게 설정
+    const x = 220 + i * 115;
+    const y = 235;
 
     Composite.add(
       stack,
       Bodies.rectangle(x, y, 40, 40, {
         render: {
           sprite: {
-            texture: box2Images[box2ImageIndex2 % box2Images.length], // 중복을 방지하기 위해 배열 크기로 나눔
+            texture: box2Images[box2ImageIndex2 % box2Images.length],
           },
         },
       })
     );
-
     box2ImageIndex2++;
   }
-  //3줄 7개
+
+  // 3줄 7개
   for (let i = 0; i < numRectangles3; i++) {
-    const x = 245 + i * 90; // x 위치를 고정
-    const y = 338; // y 위치를 조절하여 15씩 차이 나게 설정
+    const x = 245 + i * 90;
+    const y = 338;
 
     Composite.add(
       stack,
       Bodies.rectangle(x, y, 40, 40, {
         render: {
           sprite: {
-            texture: box3Images[box3ImageIndex3 % box3Images.length], // 중복을 방지하기 위해 배열 크기로 나눔
+            texture: box3Images[box3ImageIndex3 % box3Images.length],
           },
         },
       })
     );
-
     box3ImageIndex3++;
   }
-  //4줄 7개
+
+  // 4줄 7개
   for (let i = 0; i < numRectangles4; i++) {
-    const x = 280 + i * 80; // x 위치를 고정
-    const y = 415; // y 위치를 조절하여 15씩 차이 나게 설정
+    const x = 280 + i * 80;
+    const y = 415;
 
     Composite.add(
       stack,
       Bodies.rectangle(x, y, 40, 40, {
         render: {
           sprite: {
-            texture: box4Images[box4ImageIndex4 % box4Images.length], // 중복을 방지하기 위해 배열 크기로 나눔
+            texture: box4Images[box4ImageIndex4 % box4Images.length],
           },
         },
       })
     );
-
     box4ImageIndex4++;
   }
-  //5줄 7개
+
+  // 5줄 7개
   for (let i = 0; i < numRectangles5; i++) {
-    const x = 308 + i * 71; // x 위치를 고정
-    const y = 478; // y 위치를 조절하여 15씩 차이 나게 설정
+    const x = 308 + i * 71;
+    const y = 478;
 
     Composite.add(
       stack,
       Bodies.rectangle(x, y, 40, 40, {
         render: {
           sprite: {
-            texture: box5Images[box5ImageIndex5 % box5Images.length], // 중복을 방지하기 위해 배열 크기로 나눔
+            texture: box5Images[box5ImageIndex5 % box5Images.length],
           },
         },
       })
     );
-
     box5ImageIndex5++;
   }
-  //6줄 7개
+
+  // 6줄 7개
   for (let i = 0; i < numRectangles6; i++) {
-    const x = 335 + i * 65; // x 위치를 고정
-    const y = 530; // y 위치를 조절하여 15씩 차이 나게 설정
+    const x = 335 + i * 65;
+    const y = 530;
 
     Composite.add(
       stack,
       Bodies.rectangle(x, y, 40, 40, {
         render: {
           sprite: {
-            texture: box6Images[box6ImageIndex6 % box6Images.length], // 중복을 방지하기 위해 배열 크기로 나눔
+            texture: box6Images[box6ImageIndex6 % box6Images.length],
           },
         },
       })
     );
-
     box6ImageIndex6++;
   }
-  //7줄 7개
+
+  // 7줄 7개
   for (let i = 0; i < numRectangles7; i++) {
-    const x = 360 + i * 58; // x 위치를 고정
-    const y = 575; // y 위치를 조절하여 15씩 차이 나게 설정
+    const x = 360 + i * 58;
+    const y = 575;
 
     Composite.add(
       stack,
       Bodies.rectangle(x, y, 30, 30, {
         render: {
           sprite: {
-            texture: box7Images[box7ImageIndex7 % box7Images.length], // 중복을 방지하기 위해 배열 크기로 나눔
+            texture: box7Images[box7ImageIndex7 % box7Images.length],
           },
         },
       })
     );
-
     box7ImageIndex7++;
   }
-  //8줄 7개
+
+  // 8줄 7개
   for (let i = 0; i < numRectangles8; i++) {
-    const x = 385 + i * 49; // x 위치를 고정
-    const y = 615; // y 위치를 조절하여 15씩 차이 나게 설정
+    const x = 385 + i * 49;
+    const y = 615;
 
     Composite.add(
       stack,
       Bodies.rectangle(x, y, 30, 30, {
         render: {
           sprite: {
-            texture: box8Images[box8ImageIndex8 % box8Images.length], // 중복을 방지하기 위해 배열 크기로 나눔
+            texture: box8Images[box8ImageIndex8 % box8Images.length],
           },
         },
       })
     );
-
     box8ImageIndex8++;
   }
-  //9줄 6개
+
+  // 9줄 6개
   for (let i = 0; i < numRectangles9; i++) {
-    const x = 418 + i * 49; // x 위치를 고정
-    const y = 650; // y 위치를 조절하여 15씩 차이 나게 설정
+    const x = 418 + i * 49;
+    const y = 650;
 
     Composite.add(
       stack,
       Bodies.rectangle(x, y, 30, 30, {
         render: {
           sprite: {
-            texture: box9Images[box9ImageIndex9 % box9Images.length], // 중복을 방지하기 위해 배열 크기로 나눔
+            texture: box9Images[box9ImageIndex9 % box9Images.length],
           },
         },
       })
     );
-
     box9ImageIndex9++;
   }
-  //10번 컬러박스 2개
+
+  // 10번 컬러박스 2개
   for (let i = 0; i < numRectangles10; i++) {
-    const x = 125 + i * 94; // x 위치를 고정
-    const y = 570; // y 위치를 조절하여 15씩 차이 나게 설정
+    const x = 125 + i * 94;
+    const y = 570;
 
     Composite.add(
       stack,
       Bodies.rectangle(x, y, 30, 30, {
-        isStatic: true,
+        isStatic: true, //고정
         render: {
           sprite: {
             texture: box10Images[box10ImageIndex10 % box10Images.length],
@@ -361,11 +378,9 @@ function setup() {
         },
       })
     );
-
     box10ImageIndex10++;
   }
-  // 만든 바디를 세계에 추가
-  Composite.add(world, stack);
+  Composite.add(world, stack); // 만든 바디를 세계에 추가
 
   // 마우스
   m = Mouse.create(document.querySelector('canvas'));
@@ -381,10 +396,13 @@ function setup() {
   background('white');
 }
 
+// 그리기
 function draw() {
   background('white');
   noStroke();
   noFill();
+
+  // 벽
   walls.forEach((eachWall) => {
     beginShape();
     eachWall.vertices.forEach((each) => {
@@ -396,12 +414,15 @@ function draw() {
     endShape(CLOSE);
   });
 
+  // 도형 이미지
   stack.bodies.forEach((eachBody) => {
     const pos = eachBody.position;
     const angle = eachBody.angle;
     push();
     translate(pos.x, pos.y);
     rotate(angle);
+
+    // 바디가 이미지를 가지고 있으면서 해당 이미지의 텍스트 정보가 있는지 확인
     if (eachBody.render.sprite && eachBody.render.sprite.texture) {
       image(
         eachBody.render.sprite.texture,
